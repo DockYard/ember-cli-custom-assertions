6657a3d0944fab782f324add5342de755ee84d53
Brian Cardarella <bcardarella@gmail.com>
2015-05-31 13:37:15 -0400

    Add assertion blueprint

---
+++ blueprints/assertion/index.js
@@ -0,0 +1,17 @@
+var stringUtil = require('ember-cli/lib/utilities/string');
+
+module.exports = {
+  description: 'Generates a new custom assertion into tests/assertions/',
+
+  locals: function(options) {
+    var entity    = options.entity;
+    var rawName   = entity.name;
+    var name      = stringUtil.dasherize(rawName);
+    var camelName = stringUtil.camelize(rawName);
+
+    return {
+      name: name,
+      camelName: camelName
+    };
+  }
+}
