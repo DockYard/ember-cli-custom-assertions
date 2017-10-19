4b5ab702e5107a9ba554bc70bafb3e4732370a81
David Tang <dtang859@gmail.com>
2017-10-18 17:46:13 -0700

    Update __name__.js
    Update the documentation link for pushResult

--- blueprints/assertion/files/tests/assertions/__name__.js
+++ blueprints/assertion/files/tests/assertions/__name__.js
@@ -1,4 +1,4 @@
 export default function <%= camelName %>(/* context, arg1, arg2 */) {
   // use this.pushResult to add the assertion.
-  // see: https://api.qunitjs.com/pushResult/ for more information
+  // see: https://api.qunitjs.com/assert/pushResult for more information
 }
